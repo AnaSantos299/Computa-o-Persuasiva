04c713ce55d44a54cf83817fc30e1a09a7038908
Felipe <felipeclevert@gmail.com>
2022-11-24 23:23:59 +0000

    Bugs fixed, design changed
    Fix some bugs On Mobile
    Fix responsiveness
    Change color
    Reduce the icons
    Reorganize the files
    Fix the fps limit
    Fix sometimes the scroll stop before
    Fix bug on changing screens

---
+++ My project/Assets/MyAssets/Scripts/FPSShow.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSShow : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _fpsText;
+    [SerializeField] private float _hudRefreshRate = 1f;
+
+    private float _timer;
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0; // VSync must be disabled.
+        Application.targetFrameRate = 60;
+    }
+    private void Update()
+    {
+        if (Time.unscaledTime > _timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            _fpsText.text = "FPS: " + fps;
+            _timer = Time.unscaledTime + _hudRefreshRate;
+        }
+    }
+}
